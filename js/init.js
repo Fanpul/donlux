637138a3d7ae69baabe78b2c8d031fef0ef6bf3e
fanpul <denis.leonov1990@gmail.com>
2015-07-29 22:06:46 +0300

    fixed bags

--- js/init.js
+++ js/init.js
@@ -3,6 +3,11 @@
 
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
-
+    var options = [
+    	{selector: '.it1', offset: 400, callback: 'Materialize.fadeInImage(".it1")' },
+    	{selector: '#cont', offset: 400, callback: '$("#cont").slideDown(1000)' },
+    	{selector: '.it', offset: 400, callback: 'Materialize.fadeInImage(".it")' }
+  	];
+  	Materialize.scrollFire(options);
   }); // end of document ready
 })(jQuery); // end of jQuery name space
