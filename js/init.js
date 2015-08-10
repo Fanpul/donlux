2d68a25546161f01061aecb2582dd734ea0efc8e
B13eL <evheniimartynenko@gmail.com>
2015-08-11 02:44:35 +0300

    Add comments

--- js/init.js
+++ js/init.js
@@ -6,7 +6,8 @@
     var options = [
     	{selector: '.it1', offset: 400, callback: 'Materialize.fadeInImage(".it1")' },
     	{selector: '#cont', offset: 400, callback: '$("#cont").slideDown(1000)' },
-    	{selector: '.it', offset: 400, callback: 'Materialize.fadeInImage(".it")' }
+    	{selector: '.it', offset: 400, callback: 'Materialize.fadeInImage(".it")' },
+      {selector: '.comment-list', offset: 800, callback: '$(".comment-list").slideDown(2000)' }
   	];
   	Materialize.scrollFire(options);
   }); // end of document ready
